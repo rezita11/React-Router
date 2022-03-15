ead49a1644bd651c40ef5f0dfecc0e016595d50e
rezitadhina <68811466+rezita11@users.noreply.github.com>
2022-03-15 11:14:49 +0700

    Add files via upload
    ini yang router

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center p-6 w-screen h-screen bg-gray-200 overflow-y-auto">
+      <div className="max-w-md">
+        <div className="flex items-center justify-center mb-4 text-xs text-gray-600 font-semibold uppercase tracking-wide">
+          <Link
+            className="mx-2 px-4 py-2 rounded-xl hover:bg-gray-100 transition-all ease-in-out"
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="mx-2 px-4 py-2 rounded-xl hover:bg-gray-100 transition-all ease-in-out"
+            to="/about"
+          >
+            About
+          </Link>
+        </div>
+
+        <div className="p-6 w-full bg-white rounded-xl shadow-lg">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
